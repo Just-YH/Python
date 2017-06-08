3df42775c7aab517c3c8636b029ace1fe5a44185
Yauheni Hurynovich <Yauheni_Hurynovich@epam.com>
2017-06-09 02:06:56 +0300

    add some features, functions, brackets, exception

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,27 +1,37 @@
 import re
-
+import math
+import sys
 
 def mul(a, b):
     return a*b
 
 
 def div(a, b):
-    return a/b
+    try:
+        a/b
+    except ZeroDivisionError:
+        print 'Division by zero!'
+        sys.exit()
+    else:
+        return a/b
 
 
 def add(a, b):
     return a+b
 
+#math.sqrt(),abs(),math.log(),math.log10(),math.sin(),math.cos(),math.tan(),math.asin(),math.acos(),math.atan(),math.atan2(),math.hypot()
 
+# Read file and write infomation in list
 with open('expressions.txt','r') as expr:
     list_expr=list(expr);
 
 print  list_expr;
-
+# Replace some mistakes(features)
 for a in range(len(list_expr)):
     expression = '('+list_expr[a]+')'
 
     expression = expression.replace('+-', '-')
+    expression = expression.replace('+(-', '(-')
     expression = expression.replace('--', '+')
     expression = expression.replace('-+', '-')
     expression = expression.replace('\n', '')
@@ -37,73 +47,112 @@ for a in range(len(list_expr)):
 
     count_mul_div = 0
     count_brackets = 0
-
+# Count brackets in the whole expression
     for i in range(len(expression)):
         if expression[i] == '(':
             count_brackets = count_brackets + 1
 
     print count_brackets
 
+# Divide expression on small brackets
     for i in range(count_brackets):
-        res_brackets = re.search('[+-]?[(][^()]+[)]', expression)
+        res_brackets = re.search('[+-]?[(][^()]*[)]', expression)
         exp_brackets = res_brackets.group()
         print "RES: %s" % exp_brackets
 
+
+# Count */ in small brackets
         for j in range(len(exp_brackets)):
             if exp_brackets[j] == '*' or exp_brackets[j] == '/':
                 count_mul_div = count_mul_div + 1
         print count_mul_div
 
+# FOR loop for functions in small bracket
+        res_functions = re.findall('(sqrt|abs|log|log10|sin|cos|tan|asin|acos|atan|atan2)([-+]?\d+[.]?\d*)', exp_brackets)
 
+        for i in range(len(res_functions)):
+            if res_functions[i][0] == 'sqrt':
+                exp_brackets = exp_brackets.replace(str(res_functions[i][0]) + str(res_functions[i][1]),
+                                                str(math.sqrt(float(res_functions[i][1]))))
+            elif res_functions[i][0] == 'abs':
+                exp_brackets = exp_brackets.replace(str(res_functions[i][0]) + str(res_functions[i][1]),
+                                                str(abs(float(res_functions[i][1]))))
+            elif res_functions[i][0] == 'log':
+                exp_brackets = exp_brackets.replace(str(res_functions[i][0]) + str(res_functions[i][1]),
+                                                str(math.log(float(res_functions[i][1]))))
+            elif res_functions[i][0] == 'log10':
+                exp_brackets = exp_brackets.replace(str(res_functions[i][0]) + str(res_functions[i][1]),
+                                                str(math.log10(float(res_functions[i][1]))))
+            elif res_functions[i][0] == 'sin':
+                exp_brackets = exp_brackets.replace(str(res_functions[i][0]) + str(res_functions[i][1]),
+                                                str(math.sin(float(res_functions[i][1]))))
+            elif res_functions[i][0] == 'cos':
+                exp_brackets = exp_brackets.replace(str(res_functions[i][0]) + str(res_functions[i][1]),
+                                                str(math.cos(float(res_functions[i][1]))))
+            elif res_functions[i][0] == 'tan':
+                exp_brackets = exp_brackets.replace(str(res_functions[i][0]) + str(res_functions[i][1]),
+                                                str(math.tan(float(res_functions[i][1]))))
+            elif res_functions[i][0] == 'asin':
+                exp_brackets = exp_brackets.replace(str(res_functions[i][0]) + str(res_functions[i][1]),
+                                                str(math.asin(float(res_functions[i][1]))))
+            elif res_functions[i][0] == 'acos':
+                exp_brackets = exp_brackets.replace(str(res_functions[i][0]) + str(res_functions[i][1]),
+                                                str(math.acos(float(res_functions[i][1]))))
+            elif res_functions[i][0] == 'atan':
+                exp_brackets = exp_brackets.replace(str(res_functions[i][0]) + str(res_functions[i][1]),
+                                                str(math.atan(float(res_functions[i][1]))))
+            elif res_functions[i][0] == 'atan2':
+                exp_brackets = exp_brackets.replace(str(res_functions[i][0]) + str(res_functions[i][1]),
+                                                str(math.atan2(float(res_functions[i][1]))))
+
+            # FOR loop for */ in small bracket
         for k in range(count_mul_div):
-            res = re.search(r'[-+]?\d+[*/][-+]?\d+', exp_brackets)
+            res = re.search(r'[-+]?\d+[.]?\d*[*/][-+]?\d+[.]?\d*', exp_brackets)
             print "RES1: %s" % res.group()
             symbols = re.split(r'[*/\s]', res.group())
             digits = re.findall(r'[*/\s]', res.group())
             print symbols
             print digits
             if '*' in digits:
-                factor1 = int(symbols[digits.index("*")])
-                factor2 = int(symbols[digits.index("*") + 1])
+                factor1 = float(symbols[digits.index("*")])
+                factor2 = float(symbols[digits.index("*") + 1])
                 product = mul(factor1, factor2)
                 print product
-                exp_product = "%s*%s" % (factor1, factor2)
+                exp_product = "%s*%s" % (symbols[digits.index("*")], symbols[digits.index("*") + 1])
+                print 'exp: '+ exp_product
                 exp_brackets= exp_brackets.replace(exp_product, str(product))
       #          print expression
             elif '/' in digits:
-                dividend = int(symbols[digits.index("/")])
-                divisor = int(symbols[digits.index("/") + 1])
+                dividend = float(symbols[digits.index("/")])
+                divisor = float(symbols[digits.index("/") + 1])
                 quotient = div(dividend, divisor)
                 print quotient
-                exp_quotient = "%s/%s" % (dividend, divisor)
+                exp_quotient = "%s/%s" % (symbols[digits.index("/")], symbols[digits.index("/") + 1])
                 exp_brackets = exp_brackets.replace(exp_quotient, str(quotient))
                 print expression
         print exp_brackets
 
         count_mul_div=0
-
-        outcome_ins = re.findall(r'[-+]?\d+',exp_brackets )
+# SUM for small bracket
+        outcome_ins = re.findall(r'[-+]?\d+[.]?\d*',exp_brackets )
         print outcome_ins
         result = outcome_ins[0]
         for i in range(len(outcome_ins) - 1):
             print outcome_ins
-            result = add(int(result), int(outcome_ins[i + 1]))
-            print result
-        exp_brackets = exp_brackets.replace(exp_brackets, exp_brackets[0]+str(result))
+            result = add(float(result), float(outcome_ins[i + 1]))
+        print result
+        print exp_brackets
+        exp_brackets = exp_brackets.replace(exp_brackets,str(result))
         exp_brackets = exp_brackets.replace('+-', '-')
         exp_brackets = exp_brackets.replace('--', '+')
-        expression = expression.replace(res_brackets.group(), str(exp_brackets))
+        if res_brackets.group()[0]=='-' or res_brackets.group()[0]=='+':
+            expression = expression.replace(res_brackets.group(), res_brackets.group()[0]+str(exp_brackets))
+        else :
+            expression = expression.replace(res_brackets.group(), str(exp_brackets))
 
-
-    outcome = re.findall(r'[-+]?\d+', expression)
-    print outcome
-    result = outcome[0]
-    for i in range(len(outcome)-1):
-        print outcome
-        result = add(int(result), int(outcome[i+1]))
-    print result
+    print expression
 
     file_result = open('result.txt','ab')
 
-    file_result.writelines(str(list_expr[a])+'='+str(result)+'\n')
+    file_result.writelines(str.strip(list_expr[a])+'='+str(expression)+'\n')
     file_result.close()
